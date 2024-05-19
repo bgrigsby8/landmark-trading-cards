b4c4c3b8e09d5919caddc19893ec59466267f33f
Brad Grigsby <bradgrigsby@Mac-mini.lan>
2024-05-19 19:44:45 -0400

    Initialization of app

---
+++ landmark-trading-cards/landmark-trading-cards/landmark_trading_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmark_trading_cardsApp.swift
+//  landmark-trading-cards
+//
+//  Created by Brad Grigsby on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct landmark_trading_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
